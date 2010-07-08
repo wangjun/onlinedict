929d007cae5853fd2ff290c79f3fc0378c7e2fb7
fallchloe <fallchloe@386b46f4-e533-11de-92e5-c13fd4602e6f>
2010-07-08 14:44:34 +0000

    Bug fix...

--- trunk/OnlineDict/options_manage.js
+++ trunk/OnlineDict/options_manage.js
@@ -7,8 +7,8 @@
     "dict_disable": ["checked", false],
     "ctrl_only": ["checked", false],
     "english_only": ["checked", true],
-		"dictionary_service":["selectedIndex",0],
-		"custom_service":["value","http://dict.cn/mini.php?q=%s"]
+    "dictionary_service":["radio",1],
+    "custom_service":["value","http://dict.cn/mini.php?q=%s"]
 };
 
 function getColorType()
@@ -33,21 +33,23 @@ function save_options()
     {
         switch (Options[key][0])
         {
-        case "color":
-            Options[key][1] = "#" + document.getElementById(key).value;
-            break;
-        case "checked":
-            Options[key][1] = document.getElementById(key).checked;
-            break;
-        case "color_type":
-            Options[key][1] = getColorType();
-            break;
-				case "selectedIndex":
-						Options[key][1] = document.getElementById(key).selectedIndex;
-						break;
-				case "value":
-						Options[key][1] = document.getElementById(key).value;
-					break;
+            case "color":
+                Options[key][1] = "#" + document.getElementById(key).value;
+                break;
+            case "checked":
+                Options[key][1] = document.getElementById(key).checked;
+                break;
+            case "color_type":
+                Options[key][1] = getColorType();
+                break;
+            case "selectedIndex":
+                Options[key][1] = document.getElementById(key).selectedIndex;
+                break;
+            case "value":
+                Options[key][1] = document.getElementById(key).value;
+                break;
+            case "radio":
+                Options[key][1] = $("[name="+key+"]:checked").first().val();
         }
 
     }
@@ -67,21 +69,39 @@ function restore_options()
         optionValue = localOptions[key];
         if (!optionValue) return;
         var element = document.getElementById(key);
+
+        if(!element){
+            element = $("[name="+key+"]");
+        }
+
         if (element)
         {
             element.value = localOptions[key][1];
             switch (localOptions[key][0])
             {
-            case "checked":
-                if (localOptions[key][1]) element.checked = true;
-                else element.checked = false;
-                break;
-						case "selectIndex":
-								if(localOptions[key][1]) element.selectIndex = localOptions[key][1];
-								else element.selectIndex = 0;
-						case "value":
-								if(localOptions[key][1]) element.value = localOptions[key][1];
-								else element.value = "";
+                case "checked":
+                    if (localOptions[key][1]) element.checked = true;
+                    else element.checked = false;
+                    break;
+                case "selectIndex":
+                    if(localOptions[key][1]) element.selectIndex = localOptions[key][1];
+                    else element.selectIndex = 0;
+                    break;
+                case "value":
+                    if(localOptions[key][1]) element.value = localOptions[key][1];
+                    else element.value = "";
+                    break;
+                case "radio":
+                    var value;
+                    if(value = localOptions[key][1]) {
+                        var query = "[name="+key+"][value="+value+"]";
+
+                        //alert(query);
+
+                        $(query).first()
+                        .attr("checked",true);
+                    }
+                    break;
             }
         }
     }
